ed45fd6446294c3e464fc3fbb84d304d56137400
zeiterjosh-cloud <zeiterjosh@gmail.com>
2026-02-27 21:33:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-const express = 
+const express=
   require("express");
-const app = express();
-app.use(express.static("public));
-const PORT = process.env.PORT || 10000;
-app.get("/",(req, res) => {
-  res.send("ZEITERSTRATS server is running");
-});
-app.listen(PORT, () =>{
-  console.log("Server is running on port" +PORT)
+const app=express();
+app.use(express.static("public"));
+const PORT=process.env.PORT
+||10000
