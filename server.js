addc5a932469c15c3c0fab6b516b657c485f4375
zeiterjosh-cloud <zeiterjosh@gmail.com>
2026-02-27 20:42:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 const express = require("express");
-require("dotenv").config();
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("ZEITERSTRATS server is running 🚀");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+app.use(express.static("public));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT);
